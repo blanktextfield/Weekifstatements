0b2925c84948166c51b7ae79bd54534ecd594bbc
Opilane TTHK <opilane@tthk.ee>
2020-09-10 12:31:03 +0300

    Week2Ifstatements

---
+++ Week2Ifstatements/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Net.Cache;
+
+namespace Week2Ifstatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+            int Age;
+            Console.WriteLine("How old are you?");
+            UserInput = Console.ReadLine();
+            Age = Int32.Parse(UserInput);
+            if (Age < 18)
+            {
+                Console.WriteLine($"you`re {Age}, you`re too young.");
+            } else if (Age > 18)
+            {
+                Console.WriteLine($"you are {Age}, you are old enough");
+            } else
+            {
+                Console.WriteLine($"Congratulations! you are {Age}");
+            }
+        }
+    }
+}
